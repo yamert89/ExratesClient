8df3f87da4be7535fa2285c5ab2d59dd072a6b7b
Пендальф Синий <softoad2@gmail.com>
2019-11-07 16:47:04 +0300

    activities created

---
+++ app/src/main/java/ru/exrates/mobile/CurrencyActivity.kt
@@ -0,0 +1,12 @@
+package ru.exrates.mobile
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class CurrencyActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
---
+++ app/src/main/java/ru/exrates/mobile/ExchangeActivity.kt
@@ -0,0 +1,11 @@
+package ru.exrates.mobile
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ExchangeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
