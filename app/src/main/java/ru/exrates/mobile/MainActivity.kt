16073556656d33b0efbe58d823582341ceec729d
Пендальф Синий <softoad2@gmail.com>
2019-11-12 13:55:14 +0300

    spinner

--- app/src/main/java/ru/exrates/mobile/MainActivity.kt
+++ app/src/main/java/ru/exrates/mobile/MainActivity.kt
@@ -2,11 +2,22 @@ package ru.exrates.mobile
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var currencyName: TextView
+    private lateinit var currencyPrice: TextView
+    private lateinit var exchangeName: TextView
+    private lateinit var currenciesRecyclerView: RecyclerView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        currencyName = findViewById(R.id.main_currency)
+        currencyPrice = findViewById(R.id.main_cur_price)
+        exchangeName = findViewById(R.id.main_exchange)
+
     }
 }
