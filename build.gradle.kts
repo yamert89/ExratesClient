735314da3f07dd148c9bd6536e7cb490901d9c84
Пендальф Синий <softoad2@gmail.com>
2019-12-01 21:10:13 +0300

    gradle migrated to kotlin dsl

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,16 +1,18 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 
 buildscript {
-    ext.kotlin_version = '1.3.60'
+
+    val kotlinVersion = "1.3.61"
+
     repositories {
         google()
         jcenter()
         
     }
     dependencies {
-        classpath 'com.android.tools.build:gradle:3.5.2'
-        classpath "org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version"
-        classpath "org.jetbrains.kotlin:kotlin-serialization:$kotlin_version"
+        classpath("com.android.tools.build:gradle:3.5.2")
+        classpath(kotlin("gradle-plugin", version = "$kotlinVersion"))
+
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
     }
@@ -23,7 +25,7 @@ allprojects {
         
     }
 }
-
-task clean(type: Delete) {
-    delete rootProject.buildDir
+tasks.register("clean", Delete::class){
+    delete(rootProject.buildDir)
 }
+
