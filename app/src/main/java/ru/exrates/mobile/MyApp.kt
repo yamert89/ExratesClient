9ef27fc231bc9a1178101dd1b454f2dc9043e480
Пендальф Синий <softoad2@gmail.com>
2019-12-20 21:01:30 +0300

    exchange return to data class

--- app/src/main/java/ru/exrates/mobile/MyApp.kt
+++ app/src/main/java/ru/exrates/mobile/MyApp.kt
@@ -1,6 +1,10 @@
 package ru.exrates.mobile
 
 import android.app.Application
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.kotlin.registerKotlinModule
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.jackson.JacksonConverterFactory
 import ru.exrates.mobile.logic.DataProvider
@@ -10,9 +14,14 @@ class MyApp(): Application(){
     val dataProvider: DataProvider = DataProvider()
     var restService: RestService
     init {
+        val logging = HttpLoggingInterceptor().apply { level = HttpLoggingInterceptor.Level.BODY }
+        val client = OkHttpClient.Builder()
+            .addInterceptor(logging).build()
+        val om = ObjectMapper()
+        om.registerKotlinModule()
         val retrofit = Retrofit.Builder()
             .baseUrl("http://localhost:8080/")
-            .addConverterFactory(JacksonConverterFactory.create())
+            .addConverterFactory(JacksonConverterFactory.create(om))
             .build()
         restService = retrofit.create(RestService::class.java)
         
--- app/src/main/java/ru/exrates/mobile/logic/entities/Exchange.kt
+++ app/src/main/java/ru/exrates/mobile/logic/entities/Exchange.kt
@@ -1,16 +1,6 @@
 package ru.exrates.mobile.logic.entities
 
-class Exchange(): java.io.Serializable{ //todo replace with data class
-    lateinit var name: String
-    lateinit var pairs: MutableList<CurrencyPair>
-    lateinit var changePeriods: List<String>
-    val showHidden: Boolean = true
-
-constructor(name: String, pairs: MutableList<CurrencyPair>, changePeriods: List<String>): this(){
-    this.name = name
-    this.pairs = pairs
-    this.changePeriods = changePeriods
-}
+data class Exchange(val name: String, val pairs: MutableList<CurrencyPair>, val changePeriods: List<String>, val showHidden: Boolean = true): java.io.Serializable{
 
     override fun toString(): String {
         return "$name | ${pairs.size} pairs"
