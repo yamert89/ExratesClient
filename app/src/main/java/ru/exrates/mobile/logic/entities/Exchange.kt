db3fb8e7c898a08740c77939c69e5ad75b232d0d
Пендальф Синий <softoad2@gmail.com>
2020-01-08 14:55:34 +0300

    historyPeriods added

--- app/src/main/java/ru/exrates/mobile/logic/entities/Exchange.kt
+++ app/src/main/java/ru/exrates/mobile/logic/entities/Exchange.kt
@@ -1,6 +1,9 @@
 package ru.exrates.mobile.logic.entities
 
-data class Exchange(val name: String, val pairs: MutableList<CurrencyPair>, val changePeriods: List<String>, val showHidden: Boolean = true): java.io.Serializable{
+data class Exchange(val name: String, val pairs: MutableList<CurrencyPair>,
+                    val changePeriods: List<String>,
+                    val showHidden: Boolean = true,
+                    val historyPeriods: Array<String>): java.io.Serializable{
 
     override fun toString(): String {
         return "$name | ${pairs.size} pairs"
